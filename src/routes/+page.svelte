38b0be6921fd19994eea97a480ce1ed307769590
Damien482 <damo482@live.co.uk>
2026-03-03 21:52:18 +0000

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Damper.devs first edit in svelte, and this is my second to hopefully see a live update.</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
